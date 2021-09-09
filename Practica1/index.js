5afe0081357edf04de8109125c63bfb3884b8992
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-09-08 22:36:31 -0500

    final commit

--- Practica1/index.js
+++ Practica1/index.js
@@ -5,35 +5,35 @@ const bodyParser = require("body-parser")
 const app = express();
 
 //Middleware
-app.use(express.static(path.join(__dirname,'public')))
-//Configurar el servidor y que sepa que es un json
+app.use(express.static(path.join(__dirname, 'public')))
+    //Configurar el servidor y que sepa que es un json
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/Alder-Lake", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views','index.html'))
+app.get("/Intel/Alder-Lake", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 });
 
-app.get("/Alder-Lake/Registro", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views','registro.html'))
+app.get("/Intel/Register", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'registro.html'))
 });
 
-app.get('/informacion/:identificador/:nombre',(req,res)=>{
-  res.send("<h1>Recibiendo datos</h1>")
-  console.log(req.query) //URL
-  console.log(req.params) //URL
-  //Valor concatenado
-  console.log("Password:" +req.query.password)
-  //Valor integrado
-  console.log(`Marca: ${req.query.marca}`)
-  console.log(req.params.identificador)
+app.get('/informacion/:identificador/:nombre', (req, res) => {
+    res.send("<h1>Recibiendo datos</h1>")
+    console.log(req.query) //URL
+    console.log(req.params) //URL
+        //Valor concatenado
+    console.log("Password:" + req.query.password)
+        //Valor integrado
+    console.log(`Marca: ${req.query.marca}`)
+    console.log(req.params.identificador)
 })
 
-app.post('/enviarInfo',(req,res)=>{
-  res.send("Informacion recibida")
-  console.log(req.body)
+app.post('/enviarInfo', (req, res) => {
+    res.send("Informacion recibida")
+    console.log(req.body)
 })
 
 app.listen(8080, () => {
-  console.log('Listening on port ' + 8080);
+    console.log('Listening on port ' + 8080);
 });
