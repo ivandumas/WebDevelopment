306442cc19ef76ebdf99b78644d4a27bf21eecdc
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-09-08 23:19:34 -0500

    final commit

--- Practica1/public/js/script.js
+++ Practica1/public/js/script.js
@@ -3,7 +3,7 @@ function func1() {
 }
 
 function func2() {
-    document.getElementById("table-specs").innerHTML = '<thead><tr><th scope="col">#</th><th scope="col">First</th><th scope="col">Last</th><th scope="col">Handle</th></tr></thead><tbody><tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td colspan="2">Larry the Bird</td><td>@twitter</td></tr></tbody>'
+    document.getElementById("table-specs").innerHTML = '<thead><tr><th scope="col">#</th><th scope="col">Skylake</th><th scope="col">Rocket Lake</th><th scope="col">Tiger Lake</th><th scope="col">Alder Lake</th></tr></thead><tbody><tr><th scope="row">Core</th><td>AVX2 support</td><td>IPC Improvement</td><td>Super Finn</td><td>Hybrid Core</td></tr><tr><th scope="row">IPC improvement</th><td>13%</td><td>19%</td><td>18%</td><td>21%</td></tr><tr><th scope="row">Platform</th><td>PC</td><td>PC</td><td>Mobile</td><td>PC</td></tr></tbody>'
 }
 
 function buttonWithLink() {
